67cc2d92c9a1c3ef7b7769e733d203f263b80f0d
Stefan Hagen <stefan@hagen.link>
2023-06-11 16:19:54 +0200

    Added version as test

--- vastaanottaa/__init__.py
+++ vastaanottaa/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.4.2+parent.2637a057'
-# [[[end]]] (checksum: 203623ce1610b41c9139f2b591ffc9ff)
+__version__ = '2023.6.11+parent.nonce-14-g01364449'
+# [[[end]]] (checksum: 13c6a6e4ff55f130ac3378cd84eeccef)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
