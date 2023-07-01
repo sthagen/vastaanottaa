8dc0bb2beca222ce8f47684dfdffd9327e9f60db
Stefan Hagen <stefan@hagen.link>
2023-07-01 18:14:05 +0200

    Derived some app specific variable values from module (well file) docstring and the place

--- vastaanottaa/__init__.py
+++ vastaanottaa/__init__.py
@@ -1,4 +1,6 @@
 """Receive (Finnish: vastaanottaa) text traversing unknown territories."""
+import pathlib
+
 # [[[fill git_describe()]]]
 __version__ = '2023.6.17+parent.g290b9c34'
 # [[[end]]] (checksum: f906d477470ad7cc9067b497c89ebd43)
@@ -6,9 +8,10 @@ __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
 
-APP_NAME = 'Receive (Finnish: vastaanottaa) text traversing unknown territories.'
-APP_ALIAS = 'vastaanottaa'
+APP_ALIAS = str(pathlib.Path(__file__).parent.name)
 APP_ENV = APP_ALIAS.upper()
+APP_NAME = locals()['__doc__']
+
 VERSION = __version__
 VERSION_INFO = __version_info__
 
